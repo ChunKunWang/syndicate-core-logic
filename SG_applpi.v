1826e72e2e22af309c269d0ff144276e674f08bf
ChunKunWang <amos76530@gmail.com>
2017-01-31 22:39:23 -0500

    Add comment and declare RespMsg and SG_req

--- SG_applpi.v
+++ SG_applpi.v
@@ -3,15 +3,15 @@ Add LoadPath "/home/amos/applpi".
 Require Import libapplpi.
 Require Import SG_applpi_string.
 
-(** Result channel **)
+(** Result channel *)
 Axiom result_chan : chan unit false.
 Definition succ : proc := OutAtom result_chan tt.
 
-(** Catch IO exception **)
+(** Catch IO exception *)
 Axiom IOexn_chan : chan unit false.
 Definition IOexn := OutAtom IOexn_chan tt.
 
-(** Catch a system failure **)
+(** Catch a system failure *)
 Axiom system_failure_chan : chan unit false.
 Definition failure := OutAtom system_failure_chan tt.
 
@@ -19,6 +19,8 @@ Definition may_fail := nuP
   (fun x => parP (InAtom x)
     (parP (OutAtom x tt) (x ?? (fun _ => OutAtom system_failure_chan tt)))).
 
+(** Manifest data plane *)
+(* c.f. protobufs/sg.proto: l.44-60 *)
 Record Manifest : Set := manifest
   { volume_id : String; 
     coordinator_id : String;
@@ -27,8 +29,35 @@ Record Manifest : Set := manifest
     file_version : String;
     size : String}.
 
+(** Definition of Syndicate requests from client *)
+(* c.f. protobufs/sg.proto: l.80-94 *)
+Inductive SG_req : Set :=
+  | req_write : String -> SG_req
+  | req_truncate: String -> SG_req
+  | req_detach: String -> SG_req
+  | req_rename: String -> SG_req
+  | req_putchunks: String -> SG_req
+  | req_deletechunks: String -> SG_req
+  | req_setxattr: String -> SG_req
+  | req_removexattr: String -> SG_req
+  | req_reload: String -> SG_req
+  | req_refresh: String -> SG_req
+  | req_rename_hint: String -> SG_req.
 
+Definition InputStream : Set := chan SG_req true.
 
+(** Response messages *)
+Inductive RespMsg : Set :=
+  | resp_write : RespMsg
+  | resp_truncate : RespMsg
+  | resp_detach : RespMsg
+  | resp_rename : RespMsg
+  | resp_putchunks : RespMsg
+  | resp_deletechunks : RespMsg
+  | resp_setxattr : RespMsg
+  | resp_removexattr : RespMsg
+  | resp_reload : RespMsg
+  | resp_refresh : RespMsg
+  | resp_rename_hint : RespMsg.
 
-
-
+Definition OutputStream := chan RespMsg true.
