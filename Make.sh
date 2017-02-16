9618ab6c8a0c438e4432266d1064a204df87bf2d
ChunKunWang <amos76530@gmail.com>
2017-02-16 10:48:24 -0500

    Change Makefile for new SG_fs.v

--- Make.sh
+++ Make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 touch depend
-coq_makefile SG_applpi_string.v SG_applpi.v > Makefile.SG_server
+coq_makefile SG_applpi_string.v SG_fs.v SG_applpi.v > Makefile.SG_server
 make -f Makefile.SG_server depend
 make -f Makefile.SG_server
 
